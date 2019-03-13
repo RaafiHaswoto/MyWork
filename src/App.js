627a07e65659bb42a3e41a94e406bbdaee468b90
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-03-13 21:35:25 +0700

    fix error

--- src/App.js
+++ src/App.js
@@ -50,13 +50,19 @@ class App extends Component {
         <h4>Build Second Largest Number of Array</h4>
         <p>
           Follow the Following Steps:
-    
-          1. Click website https://elastic-johnson-911bdc.netlify.com/ 2. You
-          put your Integer(s)
+          <br />
+          1. Click website https://elastic-johnson-911bdc.netlify.com/ 
+          <br />
+          2. You put your Integer(s)
+          <br />
           3. For example:
+          <br />
             a. write number 3 and click Add
+            <br />
             b. write number -2 and click Add
+            <br />
             c. For the RESULT click Add one more time
+            <br />
           4. Finished
         </p>
       </div>
